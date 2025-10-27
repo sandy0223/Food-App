31caa43bc1adb6991d54189c615304f7d438860b
Sandeep Behera <thesbcode@gmail.com>
2025-10-28 02:25:28 +0530

    model,controller,routes added

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+
+const userSchema = new mongoose.Schema({
+    fullName:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    timeStamp:{
+        type:Date,
+        default:Date.now
+    }
+})
+
+const userModel = mongoose.model('user', userSchema);
+module.exports = userModel;
