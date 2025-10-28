f4c124424b7f9ceff9100e6c16e7d9e38f5f16c9
Sandeep Behera <thesbcode@gmail.com>
2025-10-29 00:07:13 +0530

    food partner middleware

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,25 @@
+const foodpartnerModel = require('../models/foodpartner.model');
+const jwt = require('jsonwebtoken');
+
+async function authFoodPartnerMiddleware(req, res, next) {
+
+    const token = req.cookies.token
+    if (!token){
+        return res.status(401).json({ message:"Please Login First!" })
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const foodPartner = await foodpartnerModel.findById(decoded.id);
+        if (!foodPartner){
+            return res.status(401).json({ message:"Please Login First!" })
+        }
+        req.user = foodPartner;
+        next();
+    } catch (error) {
+        return res.status(401).json({ message:"Please Login First!" })
+    }
+}
+
+module.exports = {
+    authFoodPartnerMiddleware
+}
